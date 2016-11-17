def6929967924d01392268729cfb564ba47b9b65
Yadav <sayadav@ptc.com>
2016-11-17 11:51:42 +0530

    adding

--- src/arrays/AlternativePositiveAndNegative.java
+++ src/arrays/AlternativePositiveAndNegative.java
@@ -2,4 +2,25 @@ package arrays;
 
 public class AlternativePositiveAndNegative {
 
+	public static void main(String[] args) {
+		int array[] = { -1, -2, -3, 1, 2, 3 };
+		int array1[] = new int[array.length];
+		int positive = 0;
+		int negative = 1;
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] < 0) {
+				array1[negative] = array[i];
+				negative += 2;
+			} else {
+				array1[positive] = array[i];
+				positive += 2;
+			}
+		}
+
+		for (int i = 0; i < array1.length; i++) {
+			System.out.print(array1[i] + " ");
+		}
+
+	}
+
 }
