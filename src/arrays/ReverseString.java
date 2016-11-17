937b3f1798d0a7cb991f58b1c9fca101579b651e
Yadav <sayadav@ptc.com>
2016-11-17 12:44:06 +0530

    adding

--- src/arrays/ReverseString.java
+++ src/arrays/ReverseString.java
@@ -5,7 +5,7 @@ public class ReverseString {
 	public static void main(String args[]) {
 		String str = "Tomorrow";
 		char[] array = str.toCharArray();
-		for (int i = 0; i < array.length/2; i++) {
+		for (int i = 0; i < array.length / 2; i++) {
 			char a = array[i];
 			char b = array[array.length - 1 - i];
 			char temp = a;
